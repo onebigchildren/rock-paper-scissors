b09fc751c3cf6cf311ee11b868cf414369bb798e
onebigchildren <sungjihwang@gmail.com>
2022-09-26 13:23:25 +0900

    Implement playRound()

--- script.js
+++ script.js
@@ -9,3 +9,36 @@ function getComputerChoice() { // Randomly return either ‘Rock’, ‘Paper’
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+
+    if ( (playerSelection.trim()).match(/^rock$/i) === true ) {
+        if ( computerSelection.match('Rock') === true ) {
+            return 'You tied!\nRock ties with rock.';
+        } else if ( computerSelection.match('Paper') === true ) {
+            return 'You lose!\nPaper beats rock.';
+        } else {
+            return 'You win!\nRock crushes scissors.';
+        }
+
+    } else if ( (playerSelection.trim()).match(/^paper$/i) === true ) {
+        if ( computerSelection.match('Rock') === true ) {
+            return 'You win!\nPaper beats rock.';
+        } else if ( computerSelection.match('Paper') === true ) {
+            return 'You tied!\nPaper ties with paper.';
+        } else {
+            return 'You lose!\nScissors chop up paper.';
+        }
+
+    } else if ( (playerSelection.trim()).match(/^scissors$/i) === true ) {
+        if ( computerSelection.match('Rock') === true ) {
+            return 'You lose!\nRock crushes scissors.';
+        } else if ( computerSelection.match('Paper') === true ) {
+            return 'You win!\nScissors chop up paper.';
+        } else {
+            return 'You tied!\nScissors tie with scissors.';
+        }
+
+    } else {
+        return 'Error! Player Selection not valid.'
+    }
+}
